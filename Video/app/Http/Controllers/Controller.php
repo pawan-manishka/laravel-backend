cd5a45634812c9f684599c05551982200e364bca
RavinduHasithanjana <ravinduceylofana@outlook.com>
2020-05-29 07:33:47 +0530

    created project and added l5 swagger

---
+++ Video/app/Http/Controllers/Controller.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ 
+ * @OA\Info(title="My First API", version="0.1")
+
+     * @OA\Get(
+     *      path="/projects",
+     *      operationId="getProjectsList",
+     *      tags={"Projects"},
+     *      summary="Get list of projects",
+     *      description="Returns list of projects",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *     
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
