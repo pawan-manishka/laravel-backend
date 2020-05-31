2e3a907c3e462cbad0c2ccbe732820907b4b1561
RavinduHasithanjana <ravinduceylofana@outlook.com>
2020-05-31 16:48:58 +0530

    added l5 swagger intial structure added

---
+++ Video/resources/api-docs/api.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Video Platform",
+ *     description="Video Uploading Platform",
+ *     @OA\Contact(
+ *         email="ravindu1997@gmail.com"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *    )
+ * )
+ */
+
+/**
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Video Upload API Server"
+ * )
+ */
---
+++ Video/resources/api-docs/upload.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ 
+ 
+     * @OA\Post(
+     *      path="/video",
+     *      operationId="getProjectsList",
+     *      tags={"Projects"},
+     *      summary="Get list of projects",
+     *      description="Returns list of projects",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *     
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
+
